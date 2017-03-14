a1d7aa78c4a460b5bdda47152a51956e94cde16e
Sind <thesind93@gmail.com>
2017-03-14 13:46:09 +0100

    improved fragmentshader to handle layers

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ SimpleOekaki
 Version
 -------
 
-0.0.4
+0.0.5
 
 
 Usage
--- SimpleOekaki.js
+++ SimpleOekaki.js
@@ -1,8 +1,8 @@
-var VERSION = "0.0.4";
+var VERSION = "0.0.5";
 
 var MIN_BRUSH_SIZE = 1;
 var MAX_BRUSH_SIZE = 31;
-var DEFAULT_BRUSH_SIZE = 3;
+var DEFAULT_BRUSH_SIZE = 1;
 
 var DEFAULT_BACKGROUND_COLOR = [1, 1, 1];
 var DEFAULT_LAYER_COLOR = [0, 0, 0];
@@ -21,6 +21,10 @@ function SimpleOekaki(div){
 	var vertexScreenResolutionUniform;
 	var vertexMousePositionUniform;
 	var vertexSizeUniform;
+	var fragmentBackgroundColorUniform;
+	var fragmentLayerOrderUniform;
+	var fragmentLayerColorsUniform;
+	var fragmentLayerVisibilityUniform;
 
 	//html
 	var holder;
@@ -34,8 +38,8 @@ function SimpleOekaki(div){
 	var backgroundColor = DEFAULT_BACKGROUND_COLOR;
 	var currentLayer = 0;
 	var layerOrder = [0, 1, 2];
-	var LayerColors = [DEFAULT_LAYER_COLOR, DEFAULT_LAYER_COLOR, DEFAULT_LAYER_COLOR];
-	var visibleLayers = [1, 1, 1];
+	var layerColors = [DEFAULT_LAYER_COLOR, DEFAULT_LAYER_COLOR, DEFAULT_LAYER_COLOR];
+	var layerVisibility = [1, 1, 1];
 
 	var paintCircle = function(mouseX, mouseY){
 		gl.useProgram(shaderProgram);
@@ -65,8 +69,12 @@ function SimpleOekaki(div){
 	var paintGL = function(){
 		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 		gl.useProgram(shaderProgram2);
-		gl.clearColor(backgroundColor[0], backgroundColor[2], backgroundColor[3], 1.0);
+		gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[3], 1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
+		gl.uniform3fv(fragmentBackgroundColorUniform, backgroundColor)
+		gl.uniform3iv(fragmentLayerOrderUniform, layerOrder);
+		gl.uniform3fv(fragmentLayerVisibilityUniform, layerVisibility);
+		gl.uniformMatrix3fv(fragmentLayerColorsUniform, false, layerColors[0].concat(layerColors[1],layerColors[2]));
 		gl.drawArrays(gl.TRIANGLE_STRIP,0,4);
 	}
 	var getShader = function(str, type) {
@@ -132,7 +140,8 @@ void main(){\n\
   vec2 movedPosition = mousePosition + vec2(0.5, 0.5) + scaledPosition;\n\
   vec2 pos = 2.0 * movedPosition / screenResolution - 1.0;\n\
   gl_Position = vec4(pos.x, -pos.y, 0.0, 1.0);\n\
-}"
+}";
+
 		var f_sh = "uniform highp vec2 screenResolution;\n\
 uniform highp vec2 mousePosition;\n\
 uniform highp float size;\n\
@@ -142,7 +151,7 @@ void main(void)\n\
   highp float dist = distance(mousePosition + 0.5, pos);\n\
   if (dist > size/2.0 - 0.2) discard;\n\
   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n\
-}"
+}";
 		console.log("vertexshader",gl.VERTEX_SHADER);
 		var vertexShader = getShader(v_sh , gl.VERTEX_SHADER);
 		var fragmentShader = getShader(f_sh , gl.FRAGMENT_SHADER);
@@ -162,18 +171,49 @@ void main(void)\n\
 		vertexMousePositionUniform= gl.getUniformLocation(shaderProgram,"mousePosition");
 		vertexSizeUniform= gl.getUniformLocation(shaderProgram,"size");
 
-		var v_sh2 = "attribute vec2 position;\n\
-varying vec2 Texcoord;\n\
-void main(void) {\n\
-  Texcoord = (position+1.0) / 2.0;\n\
-  gl_Position = vec4(position, 0.0, 1.0);\n\
+		var v_sh2 = "attribute vec2 position; \n\
+varying vec2 Texcoord; \n\
+void main(void) { \n\
+  Texcoord = (position+1.0) / 2.0; \n\
+  gl_Position = vec4(position, 0.0, 1.0); \n\
 }";
 
-		var f_sh2 = " varying highp vec2 Texcoord;\n\
-uniform highp sampler2D imageTex;\n\
-void main(void){\n\
-  highp vec4 texColor = texture2D(imageTex,Texcoord);\n\
-  gl_FragColor = texColor;\n\
+		var f_sh2 = "precision highp float;\n\
+\n\
+varying vec2 Texcoord; \n\
+uniform vec3 backgroundColor; \n\
+uniform sampler2D imageTex; \n\
+\n\
+uniform ivec3 layerOrder; \n\
+uniform vec3 layerVisibility; \n\
+uniform mat3 layerColors; \n\
+\n\
+void main(void){ \n\
+  vec4 texColor = texture2D(imageTex,Texcoord); \n\
+\n\
+  vec3 outputColor = backgroundColor; \n\
+\n\
+  for(int i = 0; i < 3; i++){ \n\
+    int currentLayer = layerOrder[i]; \n\
+\n\
+	if(currentLayer == 0){ \n\
+      vec3 currentColor = layerColors[0]; \n\
+      float currentSet = texColor[0]; \n\
+      currentSet = currentSet * layerVisibility[0]; \n\
+      outputColor = currentSet * currentColor + outputColor * (1.0-currentSet); \n\
+    } else if(currentLayer == 1){ \n\
+      vec3 currentColor = layerColors[1];\n\
+      float currentSet = texColor[1];\n\
+      currentSet = currentSet * layerVisibility[1]; \n\
+      outputColor = currentSet * currentColor + outputColor * (1.0-currentSet);\n\
+    } else { \n\
+      vec3 currentColor = layerColors[2];\n\
+      float currentSet = texColor[2];\n\
+      currentSet = currentSet * layerVisibility[2]; \n\
+      outputColor = currentSet * currentColor + outputColor * (1.0-currentSet);\n\
+    } \n\
+  } \n\
+  gl_FragColor = vec4(outputColor,1.0);\n\
 }";
 		var vertexShader2 = getShader(v_sh2 , gl.VERTEX_SHADER);
 		var fragmentShader2 = getShader(f_sh2 , gl.FRAGMENT_SHADER);
@@ -187,6 +227,11 @@ void main(void){\n\
 		}
 		vertexPositionAttribute2 = gl.getAttribLocation(shaderProgram2, "position");
 		gl.enableVertexAttribArray(vertexPositionAttribute2);
+
+		fragmentBackgroundColorUniform = gl.getUniformLocation(shaderProgram2, "backgroundColor");
+		fragmentLayerOrderUniform = gl.getUniformLocation(shaderProgram2, "layerOrder");
+		fragmentLayerColorsUniform = gl.getUniformLocation(shaderProgram2, "layerColors");
+		fragmentLayerVisibilityUniform = gl.getUniformLocation(shaderProgram2, "layerVisibility");
 	}
 
 	var initializeGL = function() {
